6014cd4a5551f742b3a97b2abb68d2b83d0103be
Rishav Raj <59872807+rishavqwerty7@users.noreply.github.com>
2026-08-31 23:53:46 +0530

    routes added

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello world")
+
+	http.HandleFunc("/", handleRoutes)
+}
+
+func handleRoutes(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("Home routes")
 }
