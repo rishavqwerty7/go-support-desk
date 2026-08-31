4d5cdc703f850d913c49c16d3eb5b4f5b9ffbcc8
Rishav Raj <59872807+rishavqwerty7@users.noreply.github.com>
2026-08-31 23:54:48 +0530

    home route added

--- main.go
+++ main.go
@@ -9,8 +9,13 @@ func main() {
 	fmt.Println("Hello world")
 
 	http.HandleFunc("/", handleRoutes)
+	http.HandleFunc("/home", handleHomeRoute)
 }
 
 func handleRoutes(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("Home routes")
+}
+
+func handleHomeRoute(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("Home routes")
 }
