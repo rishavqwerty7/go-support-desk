7a906faeeed7fcb8329762d7d11ed8b9c8a9918e
Rishav Raj <59872807+rishavqwerty7@users.noreply.github.com>
2026-09-20 23:47:01 +0530

    tickethandler added

--- handlers/ticket_handler.go
+++ handlers/ticket_handler.go
@@ -1,6 +1,13 @@
 package handlers
 
-import "github.com/rishavqwerty7/go-support-desk/services"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/rishavqwerty7/go-support-desk/models"
+	"github.com/rishavqwerty7/go-support-desk/services"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type TicketHandler struct {
 	service *services.TicketService
@@ -11,4 +18,28 @@ func NewTicketHandler(service *services.TicketService) *TicketHandler {
 	return &TicketHandler{
 		service: service,
 	}
+}
+
+func (handler *TicketHandler) CreateHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Status method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var ticket models.CreateTicketRequest
+
+	err := json.NewDecoder(r.Body).Decode(&ticket)
+
+	if err != nil {
+		http.Error(w, "Inavlid request body", http.StatusBadRequest)
+	}
+
+	customerId := primitive.NewObjectID()
+	err = handler.service.CreateTicket(r.Context(), ticket, customerId)
+
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
 }
