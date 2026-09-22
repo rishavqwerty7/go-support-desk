7af8f16eea1b68abb1a08e305a006d88af39816a
Rishav Raj <59872807+rishavqwerty7@users.noreply.github.com>
2026-09-22 23:43:58 +0530

    get ticket by id handler added

--- handlers/ticket_handler.go
+++ handlers/ticket_handler.go
@@ -3,10 +3,12 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/rishavqwerty7/go-support-desk/models"
 	"github.com/rishavqwerty7/go-support-desk/services"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type TicketHandler struct {
@@ -70,13 +72,48 @@ func (handler *TicketHandler) GetAllTicketsHandler(w http.ResponseWriter, r *htt
 
 func (handler *TicketHandler) TicketHandler(w http.ResponseWriter, r *http.Request) {
 
-	switch r.Method {
-	case http.MethodGet:
+	switch {
+	case r.Method == http.MethodGet && r.URL.Path == "/tickets":
 		handler.GetAllTicketsHandler(w, r)
-	case http.MethodPost:
+	case r.Method == http.MethodGet && strings.HasPrefix(r.URL.Path, "/tickets/"):
+		handler.GetTicketByIdHandler(w, r)
+	case r.Method == http.MethodPost && r.URL.Path == "/tickets":
 		handler.CreateTicketHandler(w, r)
 	default:
 		http.Error(w, "Methodd not allowed", http.StatusMethodNotAllowed)
 	}
 
+}
+
+func (handler *TicketHandler) GetTicketByIdHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "invalide method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	id := strings.TrimPrefix(r.URL.Path, "/tickets/")
+
+	objectId, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		http.Error(w, "Invalid ticket id", http.StatusBadRequest)
+		return
+	}
+
+	res, err := handler.service.GetTicketById(r.Context(), objectId)
+
+	if err != nil {
+
+		if err == mongo.ErrNoDocuments {
+			http.Error(w, "Ticket not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(res)
 }
